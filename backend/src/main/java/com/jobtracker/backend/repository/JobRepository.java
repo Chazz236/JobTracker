c4856a4028f953c9591acef9b67aab08b8fba868
Chazz236 <197831996+Chazz236@users.noreply.github.com>
2026-03-31 14:21:07 -0400

    feat: add JobRepository interface

---
+++ backend/src/main/java/com/jobtracker/backend/repository/JobRepository.java
@@ -0,0 +1,9 @@
+package com.jobtracker.backend.repository;
+
+import com.jobtracker.backend.model.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long> {
+}
